6020ed70920b3515b5a6defbfb5397a7f7e0d680
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-09 02:08:31 -0500

    ➕ADD: The routes folder was created to handle our routes
    In the routes section, three main routes were added for our project

---
+++ backend/routes/user.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const express = require('express');
+const UserController = require('../controllers/user');
+
+const api = express.Router();
+
+api.get('/home', UserController.home);
+api.get('/test', UserController.test);
+api.post('/register', UserController.saveUser);
+
+module.exports = api;
