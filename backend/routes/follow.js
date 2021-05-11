f16c774f3d391a7f38db08a99037d73dd5de7fd4
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-10 21:25:42 -0500

    ➕ADD: List of users that I follow
    The method was created that allows to list the list of users that a user follows in the follow.js controller

--- backend/routes/follow.js
+++ backend/routes/follow.js
@@ -8,5 +8,6 @@ const md_auth = require('../middlewares/authenticated');
 
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
+api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
 
 module.exports = api;
