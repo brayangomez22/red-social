0ae676955d73560dcdbab1c29450643d5ced39dd
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-10 19:33:15 -0500

    ➕ADD: Follow a user
    In the follow.js controller the method was created to be able to follow the users

--- backend/controllers/follow.js
+++ backend/controllers/follow.js
@@ -6,12 +6,22 @@ const mongoosePagination = require('mongoose-pagination');
 const fs = require('fs');
 const path = require('path');
 
-const test = (req, res) => {
-    res.status(200).send({
-        message: 'Test follow',
+function saveFollow (req, res) {
+    const params = req.body;
+
+    const follow = new Follow();
+    follow.user = req.user.sub;
+    follow.followed = params.followed;
+
+    follow.save((err, followStored) => {
+        if (err) return res.status(500).send({ message: 'error saving trace' });
+
+        if (!followStored) return res.status(404).send({ message: 'tracking has not been saved' });
+
+        return res.status(200).send({ follow: followStored });
     });
 }
 
 module.exports = {
-    test
+	saveFollow,
 }
--- backend/routes/follow.js
+++ backend/routes/follow.js
@@ -6,6 +6,6 @@ const FollowController = require('../controllers/follow');
 const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
-api.get('/tests-follow', md_auth.ensureAuth, FollowController.test);
+api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 
 module.exports = api;
