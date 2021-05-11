f522c0c59f452bbb103f432679e3d357e9c4baaf
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-10 21:43:35 -0500

    ➕ADD: Followers list
    The method was created that allows to list the list of users that follow me as a user in the follow.js controller

--- backend/controllers/follow.js
+++ backend/controllers/follow.js
@@ -63,8 +63,39 @@ function getFollowingUsers(req, res) {
     });
 }
 
+function getFollowedUsers(req, res) {
+	var userId = req.user.sub;
+
+	if (req.params.id && req.params.page) {
+		userId = req.params.id;
+	}
+
+	var page = 1;
+
+	if (req.params.page) {
+		page = req.params.page;
+	} else {
+		page = req.params.id;
+	}
+
+	const itemsPerPage = 4;
+
+	Follow.find({ followed: userId }).populate('user').paginate(page, itemsPerPage, (err, follows, total) => {
+        if (err) return res.status(500).send({ message: 'server error' });
+
+        if (!follows) return res.status(404).send({ message: 'no user follows you' })
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total / itemsPerPage),
+            follows,
+        })
+    });
+}
+
 module.exports = {
     saveFollow,
     deleteFollow,
-    getFollowingUsers
+    getFollowingUsers,
+    getFollowedUsers
 }
--- backend/routes/follow.js
+++ backend/routes/follow.js
@@ -9,5 +9,6 @@ const md_auth = require('../middlewares/authenticated');
 api.post('/follow', md_auth.ensureAuth, FollowController.saveFollow);
 api.delete('/follow/:id', md_auth.ensureAuth, FollowController.deleteFollow);
 api.get('/following/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowingUsers);
+api.get('/followed/:id?/:page?', md_auth.ensureAuth, FollowController.getFollowedUsers);
 
 module.exports = api;
