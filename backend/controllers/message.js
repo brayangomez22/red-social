31c4eb921b3256a1abb731f092b0a1c3bcd68304
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-12 18:54:02 -0500

    ➕ADD: Received messages
    In the message.js controller a method was created that allows to list all the messages that a user has received

--- backend/controllers/message.js
+++ backend/controllers/message.js
@@ -25,8 +25,32 @@ function saveMessage(req, res) {
 
         return res.status(200).send({ message: messageStored });
     });
- }
+}
+
+function getReceivedMessages(req, res) {
+    const userId = req.user.sub;
+
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    }
+
+    var itemsPerPage = 4;
+
+    Message.find({ receiver: userId }).populate('emitter').paginate(page, itemsPerPage, (err, messages, total) => {
+        if (err) return res.status(500).send({ message: 'request error' });
+
+        if (!messages) return res.status(404).send({ message: 'no messages' });
+        
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total / itemsPerPage),
+            messages
+        });
+    });
+}
 
 module.exports = {
-    saveMessage
+    saveMessage,
+    getReceivedMessages
 }
--- backend/routes/message.js
+++ backend/routes/message.js
@@ -7,6 +7,7 @@ const api = express.Router();
 const md_auth = require('../middlewares/authenticated');
 
 api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
+api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
 
 module.exports = api;
 
