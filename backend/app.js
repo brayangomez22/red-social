81288099c7b8351ce9cf38a2d139a481c83db5f9
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-12 19:39:07 -0500

    ➕ADD: Configure HTTP headers and CORS access
    The HTTP headers and access to the CORS were configured to avoid errors when making requests from the front

--- backend/app.js
+++ backend/app.js
@@ -15,6 +15,14 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // CORS 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+    next();
+});
 
 // ROUTES 
 app.use('/api', user_routes);
