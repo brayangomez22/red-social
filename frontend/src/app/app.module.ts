baea2e2b37f3e1c92f156d8221e53c3e4cab56ff
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-13 21:17:43 -0500

    ➕ADD: Registration page layout
    In the component register.ts the form for the registration was laid out

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { routing, appRoutingProviders } from './app.routing';
 
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
@@ -14,6 +17,8 @@ import { RegisterComponent } from './components/register/register.component';
     ],
     imports: [
         BrowserModule,
+        FormsModule,
+        HttpClientModule,
         routing
     ],
     providers: [
--- frontend/src/app/components/register/register.component.ts
+++ frontend/src/app/components/register/register.component.ts
@@ -1,20 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from 'src/app/models/user';
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+    selector: 'app-register',
+    templateUrl: './register.component.html',
+    styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
     public title: string;
+    public user: User;
 
-    constructor() {
+    constructor(
+        private _route: ActivatedRoute,
+        private _router: Router
+    ) {
         this.title = 'Registrate';
+        this.user = new User(
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "ROLE_USER",
+            ""
+        );
     }
 
     ngOnInit(): void {
         console.log("Componente de registro cargando...");
     }
 
+    onSubmit() {
+        console.log(this.user);
+    }
 }
