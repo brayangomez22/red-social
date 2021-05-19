1543364a3d64a1fd9759b319ea1dd86e52bd637a
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-18 22:58:53 -0500

    ➕ADD: Upload avatars
    The user-edit.ts component implements the logic to upload user avatars

--- frontend/src/app/components/user-edit/user-edit.component.ts
+++ frontend/src/app/components/user-edit/user-edit.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { Router, ActivatedRoute, Params } from '@angular/router';
-import { UserService } from 'src/app/services/user.service'; 
+import { UserService } from 'src/app/services/user.service';
+import { UploadService } from 'src/app/services/upload.service';
+import { GLOBAL } from 'src/app/services/global';
 
 @Component({
     selector: 'app-user-edit',
     templateUrl: './user-edit.component.html',
     styleUrls: ['./user-edit.component.scss'],
-    providers: [ UserService ]
+    providers: [ UserService, UploadService ]
 })
 export class UserEditComponent implements OnInit {
 
@@ -16,17 +18,22 @@ export class UserEditComponent implements OnInit {
     public user: User;
     public token;
     public status: string;
+    public filesToUpload: Array<File>;
+    public url: string;
 
     constructor(
         private _route: ActivatedRoute,
         private _router: Router,
-        private _useService: UserService
+        private _useService: UserService,
+        private _uploadService: UploadService
     ) {
+        this.filesToUpload = [];
         this.status = '';
         this.title = 'Update my data';
         this.user = this._useService.getIdentity();
         this.identity = this.user;
         this.token = this._useService.getToken();
+        this.url = GLOBAL.url;
     }
 
     ngOnInit(): void {
@@ -43,6 +50,12 @@ export class UserEditComponent implements OnInit {
                     this.status = 'success';
                     localStorage.setItem('identity', JSON.stringify(this.user));
                     this.identity = this.user;
+                    this._uploadService.makeFileRequest(
+                        this.url + 'upload-image-user/' + this.user._id, [], this.filesToUpload, this.token, 'image'
+                    ).then((result: any) => {
+                        this.user.image = result.user.image;
+                        localStorage.setItem('identity', JSON.stringify(this.user));
+                    });
                 }
             },
             error => {
@@ -54,5 +67,9 @@ export class UserEditComponent implements OnInit {
                 }
             }
         );
+    }
+
+    fileChangeEvent(fileInput: any) {
+        this.filesToUpload = <Array<File>>fileInput.target.files;
     }
 }
