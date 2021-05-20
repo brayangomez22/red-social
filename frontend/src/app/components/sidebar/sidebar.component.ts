d3ac6be2aeae38a02f5d20a8febddfbd78d5d4d4
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-19 21:10:29 -0500

    ➕ADD: Create posts
    The service was created to create the publications

--- frontend/src/app/components/sidebar/sidebar.component.ts
+++ frontend/src/app/components/sidebar/sidebar.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/services/user.service';
 import { Publication } from 'src/app/models/publication';
 import { GLOBAL } from 'src/app/services/global';
+import { PublicationService } from 'src/app/services/publication.service';
 
 @Component({
     selector: 'app-sidebar',
     templateUrl: './sidebar.component.html',
     styleUrls: ['./sidebar.component.scss'],
-    providers: [ UserService ]
+    providers: [ UserService, PublicationService ]
 })
 export class SidebarComponent implements OnInit {
 
@@ -19,7 +20,8 @@ export class SidebarComponent implements OnInit {
     public publication: Publication;
 
     constructor(
-        private _userService: UserService
+        private _userService: UserService,
+        private _publicationService: PublicationService
     ) {
         this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
@@ -31,7 +33,25 @@ export class SidebarComponent implements OnInit {
     ngOnInit(): void {
     }
 
-    onSubmit() {
-        console.log(this.publication);   
+    onSubmit(form) {
+        this._publicationService.addPublication(this.token, this.publication).subscribe(
+            response => {
+                if (response.publication) {
+                    // this.publication = response.publication;
+                    this.status = 'success';
+                    form.reset();
+                } else {
+                    this.status = 'error';
+                }
+            },
+            error => {
+                let errorMessage = <any>error;
+                console.log(errorMessage);
+
+                if (errorMessage != null) {
+                    this.status = 'error';
+                }
+            }
+        );
     }
 }
---
+++ frontend/src/app/services/publication.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { GLOBAL } from './global';
+import { Publication } from '../models/publication'; 
+
+@Injectable({
+    providedIn: 'root'
+})
+export class PublicationService {
+
+    public url: string;
+
+    constructor(private _http: HttpClient) {
+        this.url = GLOBAL.url;
+    }
+
+    addPublication(token, publication): Observable<any> {
+        let params = JSON.stringify(publication);
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                    .set('Authorization', token);
+                                    
+        return this._http.post(this.url + 'publication', params, { headers: headers });
+    }
+}
