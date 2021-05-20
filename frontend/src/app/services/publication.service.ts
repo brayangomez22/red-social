beea2c30591cd51008edda2513deb82cd9d4b161
brayangomez22 <gomezmancobrayanalexander@gmail.com>
2021-05-19 21:17:23 -0500

    ➕ADD: New methods in services
    The service was created to bring the publications and delete them

--- frontend/src/app/services/publication.service.ts
+++ frontend/src/app/services/publication.service.ts
@@ -22,4 +22,19 @@ export class PublicationService {
                                     
         return this._http.post(this.url + 'publication', params, { headers: headers });
     }
+
+    getPublications(token, page = 1): Observable<any> {
+        let headers = new HttpHeaders()
+            .set('Content-Type', 'application/json')
+            .set('Authorization', token);
+
+        return this._http.get(this.url + 'publications/'+page, { headers: headers });
+    }
+
+    deletePublication(token, id): Observable<any> {
+        let headers = new HttpHeaders().set('Content-Type', 'application/json')
+                                    .set('Authorization', token);
+                                    
+        return this._http.delete(this.url + 'publication/'+id, { headers: headers });
+    }
 }
